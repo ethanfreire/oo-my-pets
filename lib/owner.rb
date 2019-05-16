e478a46659e058edb5e0284181f03e9fd6f9175b
Your Name <you@example.com>
2019-05-16 17:36:25 -0400

    done

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
-class Owner
+class Cat
   # code goes here
+attr_accessor :mood, :owner
+  #a getter get cats' current mood
+  attr_reader :name
+  #an array to save all instances of cats into class
+  @@cats = []
+
+#when we create a instance of Cats object we need cats to have ...
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@cats << self
+  end
+
+  #defining a variable called mood for cats mood
+  def mood=(current_mood)
+    @mood = current_mood
+  end
+
+
+
+  # return all the cats in the cats' array
+  def self.all
+    @@cats
+  end
+
 end
